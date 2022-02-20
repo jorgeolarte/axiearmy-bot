fd9c68acb3f23cee72b95afdcc13e90b95a9df79
Jorge Olarte <ingeniero@jorgeolarte.com>
2022-02-19 20:07:46 -0500

    Styling rank command 🤓

--- commands/top.js
+++ commands/top.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { bestTrainers } = require("../lib/teamStats");
 
@@ -20,19 +21,49 @@ module.exports = {
 
     let trainers = await bestTrainers(option);
 
-    let message = "";
-
-    message = `Los mejores entrenadores por ${option.toUpperCase()} \n\n`;
+    let messageReply = {
+      title: `Best trainers by ${option.toUpperCase()}`,
+      content: `Discover who are our best trainers`,
+      fields: [],
+    };
 
     trainers.map((trainer, index) => {
-      message += index === 0 ? `>>> ` : ``;
-      message += `**${index + 1}.** `;
-      message += `${trainer.name} | ${trainer.team} | ${trainer[option]}`;
-      message += index === 0 ? ` :trophy: \n` : `\n`;
+      let text = concatTrainerOption(index, trainer[option]);
+
+      messageReply.fields.push({
+        name: `${index + 1}. ${trainer.name}`,
+        value: `${text}`,
+      });
     });
 
-    await interaction.reply({
-      content: `${message}`,
-    });
+    await replyMessage(interaction, messageReply);
   },
 };
+
+function concatTrainerOption(index, option) {
+  let text = index === 0 ? `:trophy: ` : ``;
+  text += `_${option}_`;
+
+  return text;
+}
+
+async function replyMessage(interaction, messageReply) {
+  const embed = new MessageEmbed()
+    .setColor("#ff95b9")
+    .setTitle(`${messageReply.title}`)
+    .setDescription(`${messageReply.content}`)
+    .addFields(messageReply.fields)
+    .setThumbnail(
+      "https://res.cloudinary.com/axiearmy-club/image/upload/v1645318880/axie-army/axie-army-logo-redes_bzpuhc.png"
+    )
+    .setTimestamp()
+    .setFooter({
+      text: "Rank by Axie Army",
+    });
+  // .setThumbnail("https://i.imgur.com/AfFp7pu.png");
+  // .setImage("https://i.imgur.com/AfFp7pu.png");
+
+  await interaction.reply({
+    embeds: [embed],
+  });
+}
